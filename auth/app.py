224bf71e26887a76137787774b372300bff55f02
Renato Regis <renatodruregis@gmail.com>
2022-12-02 19:00:18 -0300

    feature/issue-21-criar-endpoint-de-cadastro (#23)

--- auth/app.py
+++ auth/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, json
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def hello_world():
     return "ola mundo"
 
+#linux      curl --data '{"usuario":"lohann","senha":"123"}' -H "Content-Type: application/json" -X POST localhost:3000/registrar
+#windows    curl.exe --data '{\"usuario\":\"lohann\",\"senha\":\"123\"}' -H "Content-Type:application/json" -X POST localhost:3000/registrar
+@app.route("/registrar", methods=['POST'])
+def registrar():
+    global users_data    
+    data = json.loads(request.data)
+    users_data[data['usuario']] = data['senha']
+    retorno = app.response_class(response="Usuario registrado com sucesso!",
+                                  status=200,
+                                  mimetype='application/json')
+    return retorno
 
 if __name__ == "__main__":
+    users_data = {}
     app.run(host="0.0.0.0", port=3000, debug=True)
