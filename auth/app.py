a080410f73ef32140ed9cb1310c7cd3bb0687145
Botelhomf <66093235+Botelhomf@users.noreply.github.com>
2022-12-03 11:11:23 -0300

    Feature/isuue 22/criação endpoint login (#28)
    Co-authored-by: Daniel <daniel.feitosa@121labs.io>
    Co-authored-by: Rafael Peres Rubira <rafaelrubira2@gmail.com>

--- auth/app.py
+++ auth/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, json
+from flask import Flask, request, json, Response
+import jwt
 
 app = Flask(__name__)
 
@@ -8,6 +9,7 @@ def hello_world():
 
 #linux      curl --data '{"usuario":"lohann","senha":"123"}' -H "Content-Type: application/json" -X POST localhost:3000/registrar
 #windows    curl.exe --data '{\"usuario\":\"lohann\",\"senha\":\"123\"}' -H "Content-Type:application/json" -X POST localhost:3000/registrar
+
 @app.route("/registrar", methods=['POST'])
 def registrar():
     global users_data    
@@ -21,3 +23,18 @@ def registrar():
 if __name__ == "__main__":
     users_data = {}
     app.run(host="0.0.0.0", port=3000, debug=True)
+
+#linux      curl --data '{"usuario":"aluno","senha":"123"}' -H "Content-Type: application/json" -X POST localhost:3000/login
+@app.route("/login", methods=['POST'])
+def login():
+    global users_data    
+    data = json.loads(request.data)
+    usuario = data['usuario']
+    senha = data['senha']
+    
+    if users_data[usuario] == senha: 
+        token_jwt = jwt.encode({"usuario": usuario}, "secret", algorithm="HS256")
+        return app.response_class(response=json.dumps({"token": token_jwt}), headers='application/json', status=200)
+    
+    else:
+        return app.response_class(body=json.dumps({"erro": "credênciais inválidas"}), headers='application/json', status=403)
