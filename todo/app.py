a897740dcd7e42b8a91781811db1b7d0837e6232
marcio.renato <ubuntu@Dynatrace.wkqxjyzv0itevgytyekkl0vsmg.nx.internal.cloudapp.net>
2022-12-03 15:01:20 +0000

    TODO_PORT

--- todo/app.py
+++ todo/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "ola mundo"
-
-
+ 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    porta = os.environ.get("TODO_PORT", 4000)
+    app.run(host="0.0.0.0", port=porta, debug=True)
