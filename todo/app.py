1a71a476456b8cb1f1beb5f8faddfe03cac9d7ba
ManicaFelipe <112133618+ManicaFelipe@users.noreply.github.com>
2022-12-03 01:14:55 -0300

    Update app.py

--- todo/app.py
+++ todo/app.py
@@ -4,15 +4,11 @@ app = Flask(__name__)
 
 @app.route('/health', methods=['GET'])
 def health():
-    
     return app.response_class(
-       response=json.dumps("status": "OK"),
+       response=json.dumps({"status": "OK"}),
        status=200,
        content-type='application/json'
     )
-
-
-
-
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
