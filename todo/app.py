dcafba51743fcce0c5a874bdd54aaab71d1b6eaf
MateusCamillo <68877201+MateusCamillo@users.noreply.github.com>
2022-12-04 17:04:51 -0300

    Todo criado (#52)
    Co-authored-by: Ubuntu <ubuntu@Jumpbox.fd45gfvthipe3dswuutvvmue4g.nx.internal.cloudapp.net>

--- todo/app.py
+++ todo/app.py
@@ -1,7 +1,34 @@
-from flask import Flask
+from flask import Flask, request, json
+import jwt
+import uuid
 
 app = Flask(__name__)
+lista_todos = {}
 
+@app.route("/todos", methods=['POST'])
+def todos():
+
+    authorization = request.headers.get('Authorization')
+    authorization = authorization.split()[-1]
+
+    usuario = jwt.decode(authorization, "secret", algorithms=["HS256"])['usuario']
+
+    id = uuid.uuid4()
+    todo = {
+        "id": id,
+        "todo": "Fazer trabalho de devops!!",
+        "concluido": False
+    }
+
+    global lista_todos
+    if usuario not in lista_todos:
+        lista_todos[usuario] = {}
+
+    lista_todos[usuario][id] = todo
+
+    response = app.response_class(status=200, response= json.dumps(todo))
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
