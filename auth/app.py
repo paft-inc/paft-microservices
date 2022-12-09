aef42692968b019001304779e92b8d1fa26cab29
Ubuntu <node@node.px2njdbucbkuhmm1pgliexpzxa.nx.internal.cloudapp.net>
2022-12-09 01:02:17 +0000

    atualização

--- auth/app.py
+++ auth/app.py
@@ -1,5 +1,16 @@
 from flask import Flask, request, json, Response
+from opentelemetry import trace
+from opentelemetry import metrics
 import jwt
+
+tracer = trace.get_tracer(__name__)
+
+meter = metrics.get_meter(__name__)
+
+# Now create a counter instrument to make measurements with
+registrar_counter = meter.create_counter(
+    "roll_counter",
+    description="The number of rolls by roll value",
 
 app = Flask(__name__)
 
@@ -13,26 +24,33 @@ users_data = {
 
 @app.route("/registrar", methods=['POST'])
 def registrar():
-    global users_data    
-    data = json.loads(request.data)
-    users_data[data['usuario']] = data['senha']
-    retorno = app.response_class(response="Usuario registrado com sucesso!",
-                                  status=200,
-                                  mimetype='application/json')
-    return retorno
+    global users_data
+    with tracer.start_as_current_span("registrar") as registrospan:   
+        data = json.loads(request.data)
+        users_data[data['usuario']] = data['senha']
+        retorno = app.response_class(response="Usuario registrado com sucesso!",
+                                    status=200,
+                                    mimetype='application/json')
+        registrospan.set_attribute("usuario.value", data['usuario'])
+        registrospan.set_attribute("senha.value", data['senha'])
+        registrar_counter.add(1, {"registrar": 1})
+        return retorno
 
 #linux      curl --data '{"usuario":"aluno","senha":"123"}' -H "Content-Type: application/json" -X POST localhost:3000/login
 @app.route("/login", methods=['POST'])
 def login():
-    global users_data    
-    data = json.loads(request.data)
-    usuario = data['usuario']
-    senha = data['senha']
-    if usuario in users_data and users_data[usuario] == senha:
-        token_jwt = jwt.encode({"usuario": usuario}, "secret", algorithm="HS256")
-        return app.response_class(response=json.dumps({"token": token_jwt}), mimetype='application/json', status=200)
-    else:
-        return app.response_class(response=json.dumps({"erro": "credênciais inválidas"}), mimetype='application/json', status=403)
+    global users_data
+    with tracer.start_as_current_span("login") as loginspan:   
+        data = json.loads(request.data)
+        usuario = data['usuario']
+        senha = data['senha']
+        if usuario in users_data and users_data[usuario] == senha:
+            token_jwt = jwt.encode({"usuario": usuario}, "secret", algorithm="HS256")
+            loginspan.set_attribute("status.value", 200)
+            return app.response_class(response=json.dumps({"token": token_jwt}), mimetype='application/json', status=200)
+        else:
+            loginspan.set_attribute("status.value", 403)
+            return app.response_class(response=json.dumps({"erro": "credênciais inválidas"}), mimetype='application/json', status=403)
 
 @app.route('/health', methods=['GET'])
 def health():
