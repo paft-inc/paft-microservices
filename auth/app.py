a424e1029bffbac36a71dca0932be948b2ce1d09
Ubuntu <ubuntu@jumpbox.3aj4omewym4edka5dooql0ktie.nx.internal.cloudapp.net>
2022-12-11 16:57:38 +0000

    Dynatrance

--- auth/app.py
+++ auth/app.py
@@ -1,5 +1,10 @@
+from opentelemetry import trace
 from flask import Flask, request, json, Response
 import jwt
+
+
+tracer = trace.get_tracer("auth-app")
+
 
 app = Flask(__name__)
 
@@ -19,7 +24,12 @@ def registrar():
     retorno = app.response_class(response="Usuario registrado com sucesso!",
                                   status=200,
                                   mimetype='application/json')
-    return retorno
+    with tracer.start_as_current_span("registrar") as registrarspan:
+        registrarspan.set_attribute("registro.usuario",data['usuario'])
+        registrarspan.set_attribute("registro.senha",data['senha'])
+        
+        return retorno
+
 
 #linux      curl --data '{"usuario":"aluno","senha":"123"}' -H "Content-Type: application/json" -X POST localhost:3000/login
 @app.route("/login", methods=['POST'])
@@ -28,11 +38,17 @@ def login():
     data = json.loads(request.data)
     usuario = data['usuario']
     senha = data['senha']
-    if usuario in users_data and users_data[usuario] == senha:
-        token_jwt = jwt.encode({"usuario": usuario}, "secret", algorithm="HS256")
-        return app.response_class(response=json.dumps({"token": token_jwt}), mimetype='application/json', status=200)
-    else:
-        return app.response_class(response=json.dumps({"erro": "credênciais inválidas"}), mimetype='application/json', status=403)
+
+    with tracer.start_as_current_span("login") as loginspan:
+        loginspan.set_attribute("login.usuario", data['usuario'])
+        loginspan.set_attribute("login.senha",data['senha'])
+        if usuario in users_data and users_data[usuario] == senha:
+            token_jwt = jwt.encode({"usuario": usuario}, "secret", algorithm="HS256")
+            loginspan.set_attribute("login.realizado", "login")
+            return app.response_class(response=json.dumps({"token": token_jwt}), mimetype='application/json', status=200)
+        else:
+            loginspan.set_attribute("login.realizado", "falhou")
+            return app.response_class(response=json.dumps({"erro": "credênciais inválidas"}), mimetype='application/json', status=403)
 
 @app.route('/health', methods=['GET'])
 def health():
